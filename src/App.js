9d6409614e4df8a1023aefba46355ad7df4559ce
mustbebuilt <martin@mustbebuilt.co.uk>
2023-03-16 14:35:34 +0000

    Add useEffect

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Content from "./components/Content";
 import AddFilm from "./components/AddFilm";
 import Search from "./components/Search";
 import Footer from "./components/Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
+  // load from localstorage or use empty array
   const [films, setFilms] = useState(
-    JSON.parse(localStorage.getItem("watchList"))
+    JSON.parse(localStorage.getItem("watchList")) || []
     // [
     //   { id: 1, filmTitle: "Star Wars", viewed: false },
     //   { id: 2, filmTitle: "Jaws", viewed: true },
@@ -14,24 +15,24 @@ function App() {
   );
   const [newFilm, setNewFilm] = useState("");
   const [search, setSearch] = useState("");
+  
+  useEffect(()=>{
+    localStorage.setItem("watchList", JSON.stringify(films));
+  }, [films])
 
   const addFilm = (film) => {
     const id = films.length ? films[films.length - 1].id + 1 : 1;
     const myNewFilm = { id, viewed: false, filmTitle: film };
     const listFilms = [...films, myNewFilm];
-    setAndSaveItems(listFilms);
+    setFilms(listFilms);
   };
 
-  const setAndSaveItems = (newFilms) => {
-    setFilms(newFilms);
-    localStorage.setItem("watchList", JSON.stringify(newFilms));
-  };
   const handleCheck = (id) => {
     console.info(id);
     const listFilms = films.map((film) =>
       film.id === id ? { ...film, viewed: !film.viewed } : film
     );
-    setAndSaveItems(listFilms);
+    setFilms(listFilms);
   };
 
   const handleSubmit = (e) => {
@@ -43,7 +44,7 @@ function App() {
 
   const handleDelete = (id) => {
     const listFilms = films.filter((film) => film.id !== id);
-    setAndSaveItems(listFilms);
+    setFilms(listFilms);
   };
   return (
     <div>
